b1fe0bad139131eb6c28618efc1f1813697d024e
Vezyth <103946403+Vezyth@users.noreply.github.com>
2024-03-15 18:10:36 +0700

    Update video_api.dart

--- lib/data/api/video_api.dart
+++ lib/data/api/video_api.dart
@@ -16,7 +16,7 @@ class APIService {
     Map<String, String> parameters = {
       'part': 'snippet, contentDetails, statistics',
       'id': channelId,
-      'key': 'AIzaSyDxTcppSGNPNTwUvYamzk46-hGgUyf8Ooo',
+      'key': 'API-Key',
     };
     Uri uri = Uri.https(
       _baseUrl,
@@ -50,7 +50,7 @@ class APIService {
       'playlistId': playlistId,
       'maxResults': '8',
       'pageToken': _nextPageToken,
-      'key': 'AIzaSyDxTcppSGNPNTwUvYamzk46-hGgUyf8Ooo',
+      'key': 'API-Key',
     };
     Uri uri = Uri.https(
       _baseUrl,
