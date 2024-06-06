a7348be5d0962d8d5ea19e3adf2b25665ea24969
Vezyth <103946403+Vezyth@users.noreply.github.com>
2024-06-06 10:49:49 +0700

    family altar card

---
+++ lib/components/my_card.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class MyCard extends StatelessWidget {
+  final Function()? onTap;
+  final String text;
+  final String imagePath;
+  const MyCard(
+      {super.key, required this.text, required this.imagePath, this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width / 2.2,
+        child: Card(
+          elevation: 5,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15),
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              ClipRRect(
+                borderRadius: BorderRadius.vertical(top: Radius.circular(15)),
+                child: Image.network(
+                  imagePath, // Replace with your image URL
+                  fit: BoxFit.cover,
+                  height: 80,
+                  width: double.infinity,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Text(
+                  text,
+                  style: TextStyle(fontSize: 16),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
